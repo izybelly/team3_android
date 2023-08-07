cce19532f1352953e1d35f6c25bde40a8119a6f4
izybelly <112574494+izybelly@users.noreply.github.com>
2023-08-07 14:25:45 +0800

    Adding app location services

---
+++ app/src/main/java/com/example/ad_team3/Home.java
@@ -0,0 +1,126 @@
+package com.example.ad_team3;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Home extends AppCompatActivity {
+
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(Home.this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    // Permission is not granted, request it
+                    ActivityCompat.requestPermissions(Home.this,
+                            new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION},
+                            LOCATION_PERMISSION_REQUEST_CODE);
+                } else {
+                    // Permission is already granted, proceed with location access
+                    accessLocation();
+                }
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted, proceed with location access
+                accessLocation();
+            } else {
+                // Permission denied, show a message or handle accordingly
+                Toast.makeText(this, "Location permission denied.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    // Method to access user's location
+    private void accessLocation() {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, new LocationListener() {
+                @Override
+                public void onLocationChanged(@NonNull Location location) {
+                    double userLat = location.getLatitude();
+                    double userLong = location.getLongitude();
+
+                    // Coordinates of Changi station (double check)
+                    double changiLat = 1.3678;
+                    double changiLong = 103.9826;
+
+                    // Coordinates of Clementi Road station (double check)
+                    double clementiLat = 1.3337;
+                    double clementiLong = 103.7768;
+
+                    // Coordinates of Sentosa station (double check)
+                    double sentosaLat = 1.27472;
+                    double sentosaLong = 103.80389;
+
+                    // Coordinates of Kranji Way station (double check)
+                    double kranjiLat = 1.4387;
+                    double kranjiLong = 103.7363;
+
+                    float[] results = new float[1];
+                    Location.distanceBetween(userLat, userLong, changiLat, changiLong, results);
+                    float distanceToChangi = results[0];
+
+                    Location.distanceBetween(userLat, userLong, clementiLat, clementiLong, results);
+                    float distanceToClementi = results[0];
+
+                    Location.distanceBetween(userLat, userLong, sentosaLat, sentosaLong, results);
+                    float distanceToSentosa = results[0];
+
+                    Location.distanceBetween(userLat, userLong, kranjiLat, kranjiLong, results);
+                    float distanceToKranji = results[0];
+
+                    String nearestLocation;
+                    if (distanceToChangi < distanceToClementi && distanceToChangi < distanceToSentosa && distanceToChangi < distanceToKranji) {
+                        nearestLocation = "Changi";
+                    } else if (distanceToClementi < distanceToSentosa && distanceToClementi < distanceToKranji) {
+                        nearestLocation = "Clementi";
+                    } else if (distanceToSentosa < distanceToKranji) {
+                        nearestLocation = "Sentosa";
+                    } else {
+                        nearestLocation = "Changi";
+                    }
+
+                    Toast.makeText(Home.this, "Nearest location: " + nearestLocation, Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+}
