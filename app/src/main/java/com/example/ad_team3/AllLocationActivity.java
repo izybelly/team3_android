2f84833440c699e0dd5a0621ac84f8b6ae6e701d
izybelly <112574494+izybelly@users.noreply.github.com>
2023-08-09 13:01:18 +0800

    Add seek bar for allLocation and nearestLocation

--- app/src/main/java/com/example/ad_team3/AllLocationActivity.java
+++ app/src/main/java/com/example/ad_team3/AllLocationActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class AllLocationActivity extends AppCompatActivity {
 
@@ -12,6 +14,29 @@ public class AllLocationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_location);
+
+        SeekBar seekBar = findViewById(R.id.numericSeekBar_all);
+        TextView messageTextView = findViewById(R.id.messageTextView_all);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                int selectedValue = i + 1;
+
+                String message = "Selecting prediction for " + selectedValue + " months";
+                messageTextView.setText(message);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
         Button submit = findViewById(R.id.submit_allloc);
         submit.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/ad_team3/NearestLocationActivity.java
+++ app/src/main/java/com/example/ad_team3/NearestLocationActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class NearestLocationActivity extends AppCompatActivity {
@@ -17,6 +18,29 @@ public class NearestLocationActivity extends AppCompatActivity {
         TextView nearestLocationTextView = findViewById(R.id.nearestLocationTextView);
         String nearestLocation = getIntent().getStringExtra("nearestLocation");
         nearestLocationTextView.setText("Your nearest location is: " + nearestLocation);
+
+        SeekBar seekBar = findViewById(R.id.numericSeekBar_near);
+        TextView messageTextView = findViewById(R.id.messageTextView_near);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                int selectedValue = i + 1;
+
+                String message = "Selecting prediction for " + selectedValue + " months";
+                messageTextView.setText(message);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
         Button submit = findViewById(R.id.submit_nearestloc);
         submit.setOnClickListener(new View.OnClickListener() {
