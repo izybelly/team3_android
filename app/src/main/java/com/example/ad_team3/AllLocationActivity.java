3c25e7d7bdd7f2e9a7b931bea391852ff07f4164
izybelly <112574494+izybelly@users.noreply.github.com>
2023-08-09 21:58:41 +0800

    Add creation of JSON obj in all location activity

--- app/src/main/java/com/example/ad_team3/AllLocationActivity.java
+++ app/src/main/java/com/example/ad_team3/AllLocationActivity.java
@@ -5,8 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.SeekBar;
 import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class AllLocationActivity extends AppCompatActivity {
 
@@ -42,7 +46,36 @@ public class AllLocationActivity extends AppCompatActivity {
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                CheckBox changi = findViewById(R.id.checkbox1);
+                CheckBox clementi = findViewById(R.id.checkbox2);
+                CheckBox sentosa = findViewById(R.id.checkbox3);
+                CheckBox kranji = findViewById(R.id.checkbox4);
+                int months = seekBar.getProgress() + 1;
 
+                boolean changiIsChecked = changi.isChecked();
+                boolean clementiIsChecked = clementi.isChecked();
+                boolean sentosaIsChecked = sentosa.isChecked();
+                boolean kranjiIsChecked = kranji.isChecked();
+
+                JSONObject requestPrediction = new JSONObject();
+                try{
+                    requestPrediction.put("months", months);
+                    if (changiIsChecked){
+                        requestPrediction.put("changi", true);
+                    }
+                    if (clementiIsChecked){
+                        requestPrediction.put("clementi", true);
+                    }
+                    if (sentosaIsChecked){
+                        requestPrediction.put("sentosa", true);
+                    }
+                    if (kranjiIsChecked){
+                        requestPrediction.put("kranji", true);
+                    }
+
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
             }
         });
     }
